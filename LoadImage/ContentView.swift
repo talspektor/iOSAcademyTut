5e82f0cae019586ba3687db0403eeb5d44788d94
Tal <talspektor.gmail.com>
2021-01-17 10:43:10 +0200

    initial commit

--- LoadImage/ContentView.swift
+++ LoadImage/ContentView.swift
@@ -5,12 +5,29 @@
 //  Created by Tal Spektor on 17/01/2021.
 //  Copyright © 2021 Tal Spektor. All rights reserved.
 //
-
+import SwURL
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        SwURL.setImageCache(type: .persistent)
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                RemoteImageView(url: URL(string: "https://iosacademy.io/assets/images/brand/icon.jpg")!, placeholderImage: Image(systemName: "house"), transition: .custom(transition: .opacity, animation: .easeIn(duration: 0.5)))
+                    .progress({ progress in
+                        Text("Loading \(progress)")
+                    })
+                    .aspectRatio(contentMode: .fit)
+                //                RemoteImageView(url: URL(string: "https://iosacademy.io/assets/images/brand/icon.jpg")!)
+//                    .aspectRatio(contentMode: .fit)
+                Text("Loadiong Image")
+            }
+        .navigationBarTitle("Welcome!")
+        }
     }
 }
 
