9280bf5c30a5086625e644474eaf9d38fd29c94a
Tal <talspektor.gmail.com>
2021-01-17 10:04:31 +0200

    Initial Commit

---
+++ LoadImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoadImage
+//
+//  Created by Tal Spektor on 17/01/2021.
+//  Copyright © 2021 Tal Spektor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
